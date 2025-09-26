3c1ee103191481089fe4de851a303594ff346e58
Michael Innerberger <innerbergerm@hhmi.org>
2025-09-25 20:29:17 -0400

    Add FastA reader

--- src/sequence_clustering/io.py
+++ src/sequence_clustering/io.py
@@ -36,6 +36,39 @@ class FastQReader:
         pass
 
 
+class FastAReader:
+    """
+    A simple FASTA file reader that yields (header, sequence) tuples.
+    """
+    def __init__(self, fastq_file: str):
+        self.fastq_file = fastq_file
+        self.total = 0
+        self.skipped = 0
+
+    def __iter__(self):
+        with open(self.fastq_file, 'r', encoding='ascii') as f:
+            while True:
+                header_line = f.readline().strip()
+                if not header_line:
+                    break
+                sequence = f.readline().strip()
+
+                if not is_valid_sequence(sequence):
+                    self.skipped += 1
+                    continue
+                self.total += 1
+
+                yield header_line, sequence
+                header = header_line[1:] if header_line.startswith('>') else header_line
+                yield header, sequence
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
 class FastAWriter:
     """
     A simple FASTA file writer.
